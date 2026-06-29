ede4feece0c3d7913564cf585abd2aa5d73b2221
anushkasrivastava21 <anushkaalternate@gmail.com>
2026-06-29 17:59:07 +0530

    fix: restore missing closing div in library layout

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -222,6 +222,7 @@ export default function LibraryPage() {
                       </button>
                     ))}
                   </div>
+                </div>
               </div>
             </div>
             {/* Randomizer */}
